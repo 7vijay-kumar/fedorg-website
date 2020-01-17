eefc67b8e3813604c514d7d4bdba22d261a671cf
Vijay Kumar <35485294+7vijay-kumar@users.noreply.github.com>
2020-01-17 13:41:55 +0530

    Design Update

--- frontend/src/Components/HomePage/homePage.jsx
+++ frontend/src/Components/HomePage/homePage.jsx
@@ -4,8 +4,34 @@ import styles from './homePage.module.scss';
 export default props => {
   return (
     <div className={styles.homePage}>
-      <img src={require('../Images/logo.jpg')} />
-      <h1>Coming Soon</h1>
+      <div className={styles.leftSection}>
+        <div className={styles.companyLogo}>
+          <img src={require('../Images/fedorg.svg')} />
+        </div>
+        <div className={styles.comingSoon}>
+          <h4>OUR WEBSITE IS</h4>
+          <h1>COMING SOON</h1>
+          <p>Meanwhile feeel free to intract with our social networks</p>
+        </div>
+        <div className={styles.socialMedia}>
+          <a href='#'>
+            <img src={require('../Images/facebook.svg')} />
+          </a>
+          <a href='#'>
+            <img src={require('../Images/twitter.svg')} />
+          </a>
+          <a href='#'>
+            <img src={require('../Images/Instagram.svg')} />
+          </a>
+          <a href='#'>
+            <img src={require('../Images/google.svg')} />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.rightSection}>
+        <img src={require('../Images/rocket.svg')} />
+      </div>
     </div>
   )
 }
