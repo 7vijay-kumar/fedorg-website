266af825a26931333ddb904ea694e18025a360bc
Vijay Kumar <35485294+7vijay-kumar@users.noreply.github.com>
2020-01-17 14:08:05 +0530

    Design improvise

--- frontend/src/Components/HomePage/homePage.jsx
+++ frontend/src/Components/HomePage/homePage.jsx
@@ -11,7 +11,7 @@ export default props => {
         <div className={styles.comingSoon}>
           <h4>OUR WEBSITE IS</h4>
           <h1>COMING SOON</h1>
-          <p>Meanwhile feeel free to intract with our social networks</p>
+          <p>Feel free to reach us out on our social networks</p>
         </div>
         <div className={styles.socialMedia}>
           <a href='#'>
